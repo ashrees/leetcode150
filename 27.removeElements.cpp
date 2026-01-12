59d8152be7ea15a83306b732395295ff0d04eb63
ashrees <flowoker1@gmail.com>
2026-01-11 23:46:33 -0500

    added implemention of 27.RemoveElements

---
+++ 27.removeElements.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int removeElement(vector<int>& nums, int val) {
+    int k{0};
+
+    for (int i = 0; i < nums.size(); i++) {
+        if ( nums[i] != val ) {
+            nums[k++] = nums[i];
+        }
+    }
+    return k;
+}
+
+int main() {
+    vector<int> nums = {3,2,2,3};
+    int val{3};
+
+    cout << "Number of Remaining Elements: " << removeElement(nums, val) << endl;
+}
