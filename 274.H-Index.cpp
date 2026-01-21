ac44301b4a5f73f4b2cbf166ff74d413c0822541
ashrees <flowoker1@gmail.com>
2026-01-20 23:59:34 -0500

    Implemented 274 H-Index.cpp

---
+++ 274.H-Index.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        sort(citations.begin(), citations.end());
+
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                return n - i;
+            }
+        }
+
+        return 0;
+    }
+
+int main() {
+    vector<int> citations = {3,0,6,1,5};
+    
+    cout << "h-index is: " << hIndex(citations) << endl;
+}
