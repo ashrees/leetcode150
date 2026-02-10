d54acc2f7072d7159da7cb718754cc15330f045f
ashrees <flowoker1@gmail.com>
2026-02-09 23:59:55 -0500

    Implemented 15 3Sum

---
+++ 15.3Sum.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> res;
+    sort(nums.begin(), nums.end());
+
+    for(int i = 0; i < nums.size(); i++) {
+    if(i > 0 && nums[i] == nums[i - 1]) {
+        continue;
+    }
+
+    int j = i + 1;
+    int k = nums.size() -1;
+
+    while(j < k) {
+        int total = nums[i] + nums[j] + nums[k];
+
+        if(total > 0) {
+            k--;
+        } else if(total < 0) {
+            j++;
+        } else {
+            res.push_back({nums[i], nums[j], nums[k]});
+            j++;
+
+            while(nums[j] == nums[j - 1] && j < k) {
+                j++;
+            }
+        }
+    }
+    }
+
+    return res;
+}
+
+int main() {
+    vector<int> nums = {-1 ,0 ,1 ,2 ,-1 ,-4};
+
+    for(vector<int> num : threeSum(nums)) {
+        for(int n : num) {
+            cout << n << " ";
+        }
+    }
+}
